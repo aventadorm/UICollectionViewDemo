4ff66194475c0b1019536cbc1729ffd6fc93fbea
aventadorm <mayur.phadte@gmail.com>
2015-09-28 10:52:02 +0530

    Initial Commit

---
+++ UICollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewDemo
+//
+//  Created by Mayur Phadte on 28/09/15.
+//  Copyright © 2015 Mayur Phadte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionViewDemo
+//
+//  Created by Mayur Phadte on 28/09/15.
+//  Copyright © 2015 Mayur Phadte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
